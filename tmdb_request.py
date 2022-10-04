8b74242a93ce1e7d1c15428d442bb1a73d8fe4b4
Chungsang Tom Lam <erinata@gmail.com>
2022-10-04 11:35:50 -0400

    request one movie

--- tmdb_request.py
+++ tmdb_request.py
@@ -1,3 +1,22 @@
-print("tmdb")
+import urllib.request
+
+import os
+
+if not os.path.exists("json_files"):
+	os.mkdir("json_files")
+
+f = open("api_key", "r")
+api_key = f.read()
+f.close()
+
+
+
+response = urllib.request.urlopen("https://api.themoviedb.org/3/movie/550?api_key="+api_key)
+print(response.read())
+
+
+
+
+
 
 
