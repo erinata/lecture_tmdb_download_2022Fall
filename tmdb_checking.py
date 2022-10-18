378478d30974ac8f7e2e45c3855ca315e60271c4
Chungsang Tom Lam <erinata@gmail.com>
2022-10-18 11:28:24 -0400

    check for missing values

--- tmdb_checking.py
+++ tmdb_checking.py
@@ -7,27 +7,55 @@ dataset = pandas.read_csv("parsed_files/tmdb_dataset.csv")
 
 print(dataset)
 print(dataset.describe())
+print("--------------")
 
 
-sub_dataset = dataset[ (dataset['budget']>0) & (dataset['revenue']>0) ]
+###  Check how many movies has vote average 0
+# dataset['zero_score'] = (dataset['vote_average'] == 0)*1
+# print(dataset['zero_score'].sum())
 
-print(sub_dataset)
-print(sub_dataset.describe())
-
-revenue = sub_dataset['revenue']
-budget = sub_dataset['budget']
-vote_average = sub_dataset['vote_average']
-
-logrevenue = numpy.log(revenue)
-logbudget = numpy.log(budget)
+### Generate new column by a condition
+# dataset['popular'] = (dataset['popularity'] > 30)*1
 
 
-plt.scatter(logbudget, logrevenue, c=vote_average)
-plt.title("Budget and Revenue")
-plt.ylabel("Revenue")
-plt.xlabel("Budget")
-plt.colorbar()
-plt.savefig('figure.png')
+### count missing values and drop them
+print(dataset.isna().sum())
+dataset = dataset.dropna(subset=['release_date'])
+print(dataset.isna().sum())
+
+
+
+
+
+
+
+
+print(dataset)
+print(dataset.describe())
+
+
+
+
+
+# sub_dataset = dataset[ (dataset['budget']>0) & (dataset['revenue']>0) ]
+
+# print(sub_dataset)
+# print(sub_dataset.describe())
+
+# revenue = sub_dataset['revenue']
+# budget = sub_dataset['budget']
+# vote_average = sub_dataset['vote_average']
+
+# logrevenue = numpy.log(revenue)
+# logbudget = numpy.log(budget)
+
+
+# plt.scatter(logbudget, logrevenue, c=vote_average)
+# plt.title("Budget and Revenue")
+# plt.ylabel("Revenue")
+# plt.xlabel("Budget")
+# plt.colorbar()
+# plt.savefig('figure.png')
 
 
 
