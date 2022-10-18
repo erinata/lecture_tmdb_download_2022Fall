767e41e5b642ae519d33a193a8d84e3395945d1a
Chungsang Tom Lam <erinata@gmail.com>
2022-10-18 12:57:52 -0400

    generate countries and genres

--- tmdb_checking.py
+++ tmdb_checking.py
@@ -18,14 +18,41 @@ print("--------------")
 # dataset['popular'] = (dataset['popularity'] > 30)*1
 
 
-### count missing values and drop them
-print(dataset.isna().sum())
-dataset = dataset.dropna(subset=['release_date'])
-print(dataset.isna().sum())
+# ### count missing values and drop them
+# print(dataset.isna().sum())
+# dataset = dataset.dropna(subset=['release_date'])
+# print(dataset.isna().sum())
+
+
+
+### Replace NaN with empty string
+dataset['genres'] = dataset['genres'].fillna("")
+dataset['production_countries'] = dataset['production_countries'].fillna("")
 
 
 
 
+# dataset['Adventure'] = (dataset["genres"].str.contains("Adventure"))*1
+# dataset['History'] = (dataset["genres"].str.contains("History"))*1
+
+
+def flatten(input_list):
+	return [item for sublist in input_list for item in sublist]
+
+def get_unique_list(series, sep):
+	input_list = [i.split(sep) for i in series]
+	return list(set(flatten(input_list)))
+	
+
+for genre in get_unique_list(dataset['genres'], ";"):
+	dataset["genre_" + genre] = (dataset["genres"].str.contains(genre))*1
+	
+	
+for country in get_unique_list(dataset['production_countries'], ";"):
+	dataset["country_" + country] = (dataset["production_countries"].str.contains(country))*1
+	
+	
+dataset['the'] = (dataset["movie_title"].str.contains("the ", case=False))*1
 
 
 
@@ -45,12 +72,13 @@ print(dataset.describe())
 # revenue = sub_dataset['revenue']
 # budget = sub_dataset['budget']
 # vote_average = sub_dataset['vote_average']
+# horror = sub_dataset['genre_Horror']
 
 # logrevenue = numpy.log(revenue)
 # logbudget = numpy.log(budget)
 
 
-# plt.scatter(logbudget, logrevenue, c=vote_average)
+# plt.scatter(logbudget, logrevenue, c=horror)
 # plt.title("Budget and Revenue")
 # plt.ylabel("Revenue")
 # plt.xlabel("Budget")
